1ae978659680a594acb499f082d0959a0df52c38
Miennc <2502mien@gmail.com>
2025-06-26 13:21:59 +0700

    Add VariableDemo.java to demonstrate variable declaration and output in Java

---
+++ src/variables/VariableDemo.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        // Khai báo biến kiểu int
+        int age = 20;
+        // Khai báo biến kiểu double
+        double salary = 1500.50;
+        // Khai báo biến kiểu String
+        String name = "Nguyen Van A";
+        // Khai báo biến kiểu boolean
+        boolean isStudent = true;
+
+        // In ra giá trị các biến
+        System.out.println("Tên: " + name);
+        System.out.println("Tuổi: " + age);
+        System.out.println("Lương: " + salary);
+        System.out.println("Là sinh viên: " + isStudent);
+    }
+}
